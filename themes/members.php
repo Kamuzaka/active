3182709657251c14f0592cc0d7c7cae9a7fb8ffa
bibilka99 <avorotyncev@evilcoders.me>
2020-07-08 13:44:40 +0300

    Сортировка актива

--- themes/members.php
+++ themes/members.php
@@ -7,65 +7,61 @@ global $themes;
     <meta charset="UTF-8">
     <title>Актив</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 <? include_once "includes/nav_menu.php"; ?>
 <section class="mt-5">
 	<div class="container">
-  <div class="row">
-      <h1>Актив</h1>
-  </div>
-  <br>
-  <div class="row mb-5">
-    <div class="col">
-      <div class="btn-group">
-  <button type="button" class="btn btn-secondary">Сортировать</button>
-  <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    <span class="sr-only">Toggle Dropdown</span>
-  </button>
-  <div class="dropdown-menu">
-    <a class="dropdown-item" href="#">Алфавиту</a>
-    <a class="dropdown-item" href="#">Баллам</a>
-    <a class="dropdown-item" href="#">Группе</a>
-    <a class="dropdown-item" href="#">Мероприятиям</a>
-  </div>
-</div>
-    </div>
-  </div>
-		<div class="row">
-			<div class="col">
-				<table class="table table-sm">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col"></th>
-      <th scope="col">Имя</th>
-      <th scope="col">Фамилия</th>
-      <th scope="col">Группа</th>
-      <th scope="col">Баллы</th>
-      <th scope="col">Мероприятия</th>
-    </tr>
-  </thead>
-  <tbody>
-  <<?php foreach($data['list'] as $key => $item):?>
-    <tr>
-      <th scope="row"><?= ($key + 1);?></th>
-      <td><img src="<?= $themes;?>img/icon.png" alt="" class="rounded-circle" style="height: 30px" ></td>
-      <td><?=$item['name2']; ?></td>
-      <td><?=$item['name1']; ?></td>
-      <td><?=$item['group_of_member']; ?></td>
-      <td>20</td>
-      <td>20</td>
-    </tr>
-  <?php endforeach;?>
-  </tbody>
-</table>
-			</div>
-
-
-		</div>
+        <div class="row">
+            <h1>Актив</h1>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col">
+                <table id="dtOrderExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col"></th>
+                        <th scope="col">Имя</th>
+                        <th scope="col">Фамилия</th>
+                        <th scope="col">Группа</th>
+                        <th scope="col">Баллы</th>
+                        <th scope="col">Мероприятия</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($data['list'] as $key => $item):?>
+                        <tr>
+                        <th scope="row"><?= ($key + 1);?></th>
+                        <td><img src="<?= $themes;?>img/icon.png" alt="" class="rounded-circle" style="height: 30px" ></td>
+                        <td><?=$item['name2']; ?></td>
+                        <td><?=$item['name1']; ?></td>
+                        <td><?=$item['group_of_member']; ?></td>
+                        <td>20</td>
+                        <td>20</td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
 	</div>
 </section>
+<script>
+    $(document).ready( function () {
+        $('#dtOrderExample').DataTable({
+            searching: false,
+            paging: false,
+            info: false
+        });
+    });
+</script>
 </body>
 </html>
